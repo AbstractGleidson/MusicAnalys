a6e089c0f2858f950de8284a07591048138316bc
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2025-11-23 12:54:07 -0300

    Versão inicial com a classe Music um pouco otimizada

---
+++ src/Music.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+from scipy.signal import spectrogram
+import numpy 
+from pathlib import Path
+
+class Music:
+    def __init__(self, data, sample_rate):
+        self._sample_rate = sample_rate # Amostragem da música
+        self._data_music = data / numpy.max(numpy.abs(data)) # Normalização das amostragens para valores no intervalo [-1, 1]
+        self._fft_values = self.musicDecomposition() # Calcula a FFT da música 
+        self._frequencies = self.musicFrequencies() # Calcula a FFT da frequência da música
+        self._music_duration = len(self._data_music) / self._sample_rate # Duracao da música em segundos
+        
+        # Parametros do espectograma
+        self.f = None
+        self.t = None
+        self.Sxx = None
+
+    def getDataMusic(self):
+        return self._data_music
+
+    def getSampleRateMusic(self):
+        return self._sample_rate
+    
+    def musicDecomposition(self):
+        # Transformada rápida de Fourier
+        fft_values = numpy.abs(numpy.fft.fft(self._data_music))
+        return fft_values
+    
+    def musicFrequencies(self):
+        # Frequências correspondentes
+        frequencies = numpy.fft.fftfreq(len(self._data_music), 1 / self._sample_rate)
+        return frequencies
+    
+    
+    def spectogram(self, name_music: str, time):
+
+        path_base = Path(__file__).parent.parent / "graphs"
+        path_espec = path_base / f"{name_music}_espectrograma.png"
+
+        # Manipula o tamanho da música para o espectograma
+        max_duration = min(min(self._music_duration, 240), time) # Permite gerar apenas o espectograma dos primeiro 4 minutos da musica
+        max_samples = max_duration * self._sample_rate # Quantidade maxima de amostras
+        data = self._data_music[:max_samples] # Fatia a quantidade de amostras, para as primeiras ate max_samples
+
+        if self.f is None:
+
+            # Gera espectograma
+            self.f, self.t, self.Sxx = spectrogram(
+                data, # amostras
+                self._sample_rate, # quantidade de amostras por segundos
+                nperseg=1024, 
+                noverlap=512,
+                scaling='density'
+            )
+
+            self.Sxx = 10 * numpy.log10(self.Sxx + 1e-10) # Converte 
+
+        # Configura o plot do espectograma
+        plt.figure(figsize=(12, 5))
+        plt.pcolormesh(self.t, self.f, self.Sxx, shading='gouraud')
+        plt.title(f"Espectrograma {name_music}")
+        plt.ylabel("Frequência (Hz)")
+        plt.xlabel("Tempo (s)")
+        plt.colorbar(label="Intensidade (dB)")
+        plt.tight_layout()
+        plt.savefig(path_espec)
+        plt.close()
+
+        return path_espec
+
+    # Gera o grafico da frequencia
+    def frequenceGraph(self, name_music: str):
+        
+        path_base = Path(__file__).parent.parent / "graphs"
+        path_fft = path_base / f"{name_music}_fft.png"
+        
+        size = len(self._fft_values)
+
+        #  Grafico FFT 
+        plt.figure(figsize=(12, 5))
+        plt.plot(self._frequencies[:size//2], self._fft_values[:size//2])
+        plt.title(f"FFT {name_music} (MP3)")
+        plt.xlabel("Frequência (Hz)")
+        plt.ylabel("Intensidade")
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(path_fft)
+        plt.close()
+        
+        return path_fft
---
+++ src/main.py
@@ -0,0 +1,69 @@
+from Music import Music
+from pathlib import Path
+import sys
+import os 
+import platform
+import librosa
+
+try:
+    music_name = sys.argv[1]
+except IndexError:
+    print("Música não informada!")
+    music_name = None
+    
+    
+command = "cls" if platform.platform() == "Windows-11-10.0.26100-SP0" else "clear"
+
+if __name__ == "__main__" and music_name is not None:
+        
+    path_music = Path(__file__).parent.parent / "assets" / music_name # path da musica
+    
+    try:
+        data_music, sample_rate = librosa.load(path_music, sr=None)
+        os.system(command)
+        isMusicLoad = True
+    except FileNotFoundError:  
+        os.system(command)
+        print("Não foi possível carregar a música da pasta assets!")
+        isMusicLoad = False
+    
+    
+    if isMusicLoad:
+        music = Music(data_music, sample_rate)
+        
+        # tratamento do name da musica
+        music_name = music_name.split(".")[0] # Pega apenas a musica sem a extensao
+        music_name = "_".join(music_name.split(" ")) # Tira os espacos e substitui por "_"
+
+        plot_response = "" # Resposta do plot 
+        while(True):
+            os.system(command)
+            
+            print(plot_response)
+            print("============== Menu =============")
+            print("1 - Gerar grafico da frenquencia")
+            print("2 - Gerar espectograma")
+            print("3 - Sair")
+            response = int(input(": "))
+            
+            if response == 1:
+                print("\nGerando gráfico...")
+                
+                path = music.frequenceGraph(music_name)
+        
+                plot_response = f"Gráfico gerado com suscesso. Salvo em: {path}"
+            
+            elif response == 2:
+                time = int(input("Quantidade de segundos: "))
+                
+                print("\nGerando espectograma...")
+                #Cria o spectograma da musica
+                path = music.spectogram(music_name, time)
+                
+                plot_response = f"Espectograma gerado com suscesso. Salvo em: {path}"
+                
+            elif response == 3:
+                break
+        
+            else:
+                plot_response = "Escolha uma opção válida!"
